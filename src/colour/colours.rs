f51a426a2e0d88489fbc9071f56cfda20a652981
GreenChild04 <106739622+GreenChild04@users.noreply.github.com>
2023-08-16 21:20:24 +1000

    Added the colour constants

---
+++ src/colour/colours.rs
@@ -0,0 +1,26 @@
+/// Defines the escape sequences required for each wanted colour
+pub struct Colours;
+
+macro_rules! def_colours {
+    ($(
+        $name:ident = $colour:expr;
+    )*) => {
+        impl Colours {
+            $(
+                pub const $name: &str = $colour;
+            )*
+        }
+    }
+}
+
+def_colours! {
+    NONE = "\x1b[0m";
+    PINK = "\x1b[35m";
+    WHITE = "\x1b[37m";
+    BLUE = "\x1b[34m";
+    GREEN = "\x1b[32m";
+    CYAN = "\x1b[36m";
+    RED = "\x1b[31m";
+    BLACK = "\x1b[30m";
+    YELLOW = "\x1b[33m";
+}
